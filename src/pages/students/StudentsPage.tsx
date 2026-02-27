0f954032ccffa7b869add5b51912bfd65cc5956a
P.Cardoso <www.pedrolucascardoso@gmail.com>
2026-02-27 10:19:50 -0300

    Update StudentsPage.tsx

--- src/pages/students/StudentsPage.tsx
+++ src/pages/students/StudentsPage.tsx
@@ -134,6 +134,7 @@ export default function StudentsPage() {
 
   useEffect(() => {
     fetchData()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const handleSubmit = async () => {
